ca081dcd1155928cadf0a1110bb0f25f9c256d04
Logan Joecks <ljoecks@gmail.com>
2026-02-08 13:49:19 -0500

    Update user-menu.tsx

--- src/components/user-menu.tsx
+++ src/components/user-menu.tsx
@@ -27,7 +27,7 @@ export default function UserMenu({
           src={photoURL || undefined}
         />
       </DropdownTrigger>
-      <DropdownMenu aria-label='Profile Actions' variant='flat'>
+      <DropdownMenu aria-label='Profile Actions!' variant='flat'>
         <DropdownItem key='profile' className='h-14 gap-2'>
           <p className='font-semibold'>Signed in as</p>
           <p className='font-semibold'>{displayName}</p>
